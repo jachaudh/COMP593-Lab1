fc5d6fe3b2480813da71fcbaa24e5467a69fc39a
jachaudh <156561167+jachaudh@users.noreply.github.com>
2024-01-16 11:23:20 -0500

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added two print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
  # Print lines from Zen of Python
 print("It's so cold outside")
-print("Doing things in a smarter way is better then harder way")
--- script2.py
+++ script2.py
@@ -1,1 +1,4 @@
+ # Print lines from Zen of Python
 print("Doing things in a smarter way is better then harder way")
+print("If the implementation is easy to explain, it may be a good idea.")
+print("Namespaces are one honking great idea -- let's do more of thoes!")
